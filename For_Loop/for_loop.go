a21a7bc432b43325ba4eca3eefa2f8872436cdd3
Prothes Barai <aspprothesbarai1619@gmail.com>
2026-03-04 09:06:40 +0600

    asp v4443

--- For_Loop/for_loop.go
+++ For_Loop/for_loop.go
@@ -23,3 +23,32 @@ func loopWithConditon(){
 		}
 	}
 }
+
+func miniProjectSimpleCalculator(){
+	var fnum,lnum float64
+	var operator string
+	fmt.Print("Enter First Number : ")
+	fmt.Scan(&fnum)
+	fmt.Print("Enter Operator : ")
+	fmt.Scan(&operator)
+	fmt.Print("Enter Second Number : ")
+	fmt.Scan(&lnum)
+
+	switch operator {
+	case "+":
+		fmt.Print(fnum+lnum)
+	case "-":
+		fmt.Print(fnum-lnum)
+	case "*":
+		fmt.Print(fnum*lnum)
+	case "/":
+		if(lnum != 0){
+			fmt.Print(fnum/lnum)
+		}else{
+			fmt.Print("You Cann't Devided By Zero!")
+		}
+	default:
+		fmt.Print("Invalid Operator!")
+	}
+
+}
