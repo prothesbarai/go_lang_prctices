abf9d2d8ca5865e47727b13a10b112e70aa927c3
Prothes Barai <aspprothesbarai1619@gmail.com>
2026-03-08 15:31:48 +0600

    asp v4444444

--- Function_with_return_value/multi_return.go
+++ Function_with_return_value/multi_return.go
@@ -5,28 +5,30 @@ import (
 )
 
 func multiReturn() {
-	var n int
-	fmt.Print("Enter Numbers Array Length : ")
-	fmt.Scan(&n)
-	numbers := make([]int, n) // Here make n size array/slice
-	for i := 0; i < n; i++ {
-		fmt.Printf("Enter Array Number Position-%d : ",i)
+	var num int
+	fmt.Print("Enter Size of Number List : ")
+	fmt.Scan(&num)
+	numbers :=  make([]int, num)
+	for i := 0; i < num; i++ {
+		fmt.Printf("Enter Number Position-%d : ",i)
 		fmt.Scan(&numbers[i])
 	}
 
-	min,max := getMinMax(numbers)
-	fmt.Printf("In this array min number is %d & max number is %d",min,max)
+
+	min,max := multiReturnFuncTask(numbers)
+	fmt.Printf("Min Number is : %d, And max number is %d",min,max)
+
 }
 
-func getMinMax(numbers []int) (int, int) {
+func multiReturnFuncTask(numbers []int) (int, int) {
 	min := numbers[0]
 	max := numbers[0]
 
-	for _, num := range numbers {
-		if num < min {
+	for _, num := range numbers{
+		if(num < min){
 			min = num
 		}
-		if num > max {
+		if(num > max){
 			max = num
 		}
 	}
